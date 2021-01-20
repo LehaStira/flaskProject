93fbdd54b4df79bc1614edf6da83743d284304cd
leha_stira <leha_stira@ukr.net>
2021-01-20 14:06:47 +0200

    Solving problem with view

--- view.py
+++ view.py
@@ -3,7 +3,6 @@ from flask import render_template
 
 
 @app.route('/')
-def hello_world():
-
+def index():
     my_str = 'Какой-то текст'
     return render_template('index.html', my_variable = my_str)
