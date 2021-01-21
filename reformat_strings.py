74189d95de6c3d3ed8c204d1218ec3c56e69e949
leha_stira <leha_stira@ukr.net>
2021-01-21 12:16:04 +0200

    reformat_strings.py to slug in models.py

---
+++ reformat_strings.py
@@ -0,0 +1,16 @@
+import re
+
+
+def delete_reapiting(s):
+    new_string = ''
+    for ind, i in enumerate(s):
+        if i == '-' and s[ind-1] == '-':
+            continue
+        else:
+            new_string+=i
+    return new_string
+
+
+def delete_last_symbol(s):
+    if s[-1] == '-':
+        return s[:-1]
